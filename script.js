42cf1fff47c98c394f7f76c530438e5e160ea39c
Mario Haefs <MarioHaefs@t-online.de>
2023-03-16 16:52:52 +0100

    changed to final Design and fixed Bugs

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let currentShape = 'cross';
 let gameOver = false;
 
 
+// Fragt ab welcher Spieler dran ist, wechselt dementsprechend Kreuz und Kreis und fragt eine Abbruchbedingung ab. //
 function fillShape(id) {
     if (!fields[id] && !gameOver) {
 
@@ -32,66 +33,115 @@ function draw() {
         if (fields[i] == 'cross') {
             document.getElementById('cross-' + i).classList.remove('d-none');
         }
+    }
+}
 
+// Hier werden alle möglichen Kombinationen abgefragt um ein Spiel zu gewinnen. //
+let winner = 0;
+
+function checkForWin() {
+
+    winOption1();
+    winOption2();
+    winOption3();
+    winOption4();
+    winOption5();
+    winOption6();
+    winOption7();
+    winOption8();
+    noWinner();
+
+    if (winner != 0) {
+        gameOver = true;
+
+        setTimeout(function () {
+            document.getElementById('game-over').classList.remove('d-none');
+            document.getElementById('restart-button').classList.remove('d-none');
+        }, 1500);
     }
 }
 
 
-function checkForWin() {
-    let winner;
-
+function winOption1() {
     if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
         winner = fields[0]
         document.getElementById('line-0').style.transform = 'scaleX(1)';
     }
+}
 
+
+function winOption2() {
     if (fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
         winner = fields[4]
         document.getElementById('line-1').style.transform = 'scaleX(1)';
     }
+}
 
+
+function winOption3() {
     if (fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) {
         winner = fields[7]
         document.getElementById('line-2').style.transform = 'scaleX(1)';
     }
+}
 
+
+function winOption4() {
     if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
         winner = fields[0]
         document.getElementById('line-4').style.transform = 'rotate(90deg) scaleX(1)';
     }
+}
 
+
+function winOption5() {
     if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
         winner = fields[1]
         document.getElementById('line-3').style.transform = 'rotate(90deg) scaleX(1)';
     }
+}
 
+
+function winOption6() {
     if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
         winner = fields[2]
         document.getElementById('line-5').style.transform = 'rotate(90deg) scaleX(1)';
     }
+}
 
+
+function winOption7() {
     if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
         winner = fields[0]
         document.getElementById('line-6').style.transform = 'rotate(45deg) scaleX(1)';
     }
+}
 
+
+function winOption8() {
     if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) {
         winner = fields[2]
         document.getElementById('line-7').style.transform = 'rotate(135deg) scaleX(1)';
     }
-
-    if (winner) {
-        gameOver = true;
-        
-        setTimeout(function () {
-           document.getElementById('game-over').classList.remove('d-none');
-           document.getElementById('restart-button').classList.remove('d-none');
-        }, 1000);
-
-    }
 }
 
 
+// Abbruchbedingung, wenn Unentschieden ist //
+function noWinner() {
+    if (fields[0] && fields[1] && fields[2] && fields[3] && fields[4] && fields[5] && fields[6] && fields[7] && fields[8] && winner === 0) {
+      gameOver = true;
+  
+      setTimeout(function () {
+        document.getElementById('game-over').classList.remove('d-none');
+        document.getElementById('restart-button').classList.remove('d-none');
+      }, 1500);
+    }
+  }
+  
+
+
+
+// Entfernt das Game Over Img, die Kreise und Kreuze, die Linien und den Button //
 function restart() {
     gameOver = false;
     fields = [];
@@ -106,4 +156,6 @@ function restart() {
     for (let j = 0; j < 8; j++) {
         document.getElementById('line-' + j).style.transform = 'scaleX(0.0)';
     }
+
+    winner = 0;
 }
